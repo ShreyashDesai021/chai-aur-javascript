da42238e1d0faa610d3b0e8646d1fc879e6d1b89
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-02 18:18:09 +0000

    refactor Prototype.js for improved readability and consistency

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -7,6 +7,7 @@ console.log(myName.trueLength); // undefined, trueLength is not a property of st
 
 let myHeros = ["thor","spiderman"];
 
+
 let heroPower = {
     thor: "hammer",
     spiderman: "sling",
@@ -14,7 +15,6 @@ let heroPower = {
     getSpiderPower: function(){
         console.log(`Spidy power is ${this.spiderman}`);
     }   
-
 };
 
 Object.prototype.shreyash = function(){
@@ -22,8 +22,7 @@ Object.prototype.shreyash = function(){
 }
 
 heroPower.shreyash(); // Shreyash is present in all objects
-
-myHeros.shreyash(); // Shreyash is present in all objects
+myHeros.shreyash();   // Shreyash is present in all objects
 
 // here what we did was we directly accessed the top level hierarchy of all objects in javascript which is Object.prototype
 // and added a method shreyash to it, now all objects in javascript will inherit this method be it strings, arrays or user-defined objects
@@ -32,8 +31,8 @@ Array.prototype.heyShreyash = function(){
     console.log(`Hey Shreyash from array`);
 }
 
-//heroPower.heyShreyash();  //here we will get error because we cant access 
-                        // heyShreyash method from heroPower object as it is below in the hierarchy 
+//heroPower.heyShreyash();  // here we will get error because we cant access 
+// heyShreyash method from heroPower object as it is below in the hierarchy 
 //myHeros.heyShreyash(); // same error
 
 // Inheritance:
@@ -54,7 +53,7 @@ const TASupport = {
     makeAssignment: 'JS assignment',
     fullTime: true,
     __proto__: TeacherSupport  // setting prototype of TASupport to TeacherSupport
-                           // now TASupport object can access properties of TeacherSupport object
+                              // now TASupport object can access properties of TeacherSupport object
 }
 
 Teacher.__proto__ = User; // setting prototype of Teacher to User
@@ -73,5 +72,5 @@ String.prototype.trueLength = function(){
 }
 
 anotherUsername.trueLength(); // True length is: 13
-"hitesh".trueLength(); // True length is: 6
-"iceTea".trueLength(); // True length is: 6
+"hitesh".trueLength();        // True length is: 6
+"iceTea".trueLength();        // True length is: 6
